50f1f5243c7fab6c995b1b6551b3dfb7bc8b0eaa
Heiko Scherrer <hscherrer@interface21.io>
2022-06-05 21:43:51 +0200

    #7 Migrated to SpringBoot 2.7.0

--- src/main/java/org/openwms/services/WebSecurityConfig.java
+++ src/main/java/org/openwms/services/WebSecurityConfig.java
@@ -15,21 +15,22 @@
  */
 package org.openwms.services;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * A WebSecurityConfig.
  *
  * @author <a href="mailto:scherrer@openwms.org">Heiko Scherrer</a>
  */
-@EnableWebSecurity
-class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class WebSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().ignoringAntMatchers("/eureka/**");
-        super.configure(http);
+        return http.build();
     }
 }
